26207d486a98b9ca54dee6fb0f2d3f63eff6c7f6
Minilik Shumete <minilikshumete@gmail.com>
2025-06-11 23:56:51 +0300

    feat: implement configuration set up and accessible way

--- config/config.go
+++ config/config.go
@@ -1,114 +1,167 @@
 package config
 
 import (
+	"encoding/json"
 	"os"
-	"strings"
 	"time"
 
-	"github.com/joho/godotenv"
-	"github.com/minilikmila/goAuth/model/enum"
-	"github.com/minilikmila/goAuth/utils/crypto"
-	log "github.com/sirupsen/logrus"
+	"github.com/minilikmila/standard-auth-go/internal/crypto"
 )
 
-type JWT struct {
-	SecretKey      string
-	PrivateKeyPath string
-	PublicKeyPath  string
-	Alg            string
-	Audience       string
-	Exp            time.Duration
-	Iss            string
-	PrivateKey     interface{}
-	PublicKey      interface{}
-	Secret         string
-	Type           string `json:"-"`
+type JWTConfig struct {
+	Aud            string        `json:"audience"`
+	Alg            string        `json:"algorithm"`
+	Exp            time.Duration `json:"expiry"`
+	Iss            string        `json:"issuer"`
+	PrivateKeyPath string        `json:"private_key_path"`
+	PublicKeyPath  string        `json:"public_key_path"`
+	Secret         string        `json:"secret"`
+	Type           string        `json:"-"`
+	privateKey     interface{}
+	publicKey      interface{}
 }
-type AppConfig struct {
-	App struct {
-		Host string
-		Port string
+
+type LockoutPolicy struct {
+	Attempts int           `json:"attempts"`
+	For      time.Duration `json:"for"`
+}
+
+type SocialAuthConfig struct {
+	ClientID string `json:"client_id"`
+	SecretID string `json:"secret_id"`
+	Enabled  bool   `json:"enabled"`
+}
+
+type Config struct {
+	Host                     string           `json:"host"`
+	Port                     int              `json:"port"`
+	DatabaseUri              string           `json:"database_uri"`
+	InstanceUrl              string           `json:"instance_url"`
+	DisableSignup            bool             `json:"disable_signup"`
+	DisableEmail             bool             `json:"disable_email"`
+	DisablePhone             bool             `json:"disable_phone"`
+	GeneieApiKey             string           `json:"genie_api_key"`
+	CloudinaryCloudName      string           `json:"cloudinary_cloud_name"`
+	CloudinaryKey            string           `json:"cloudinary_key"`
+	CloudinarySecret         string           `json:"cloudinary_secret"`
+	AccessTokenCookieName    string           `json:"access_token_cookie_name"`
+	AccessTokenCookieDomain  string           `json:"access_token_cookie_domain"`
+	RefreshTokenCookieName   string           `json:"refresh_token_cookie_name"`
+	RefreshTokenCookieDomain string           `json:"refresh_token_cookie_domain"`
+	SessionCookieName        string           `json:"session_cookie_name"`
+	SessionCookieDomain      string           `json:"session_cookie_domain"`
+	JWT                      JWTConfig        `json:"jwt"`
+	Google                   SocialAuthConfig `json:"google"`
+	Github                   SocialAuthConfig `json:"github"`
+	Linkedin                 SocialAuthConfig `json:"linkedin"`
+	Facebook                 SocialAuthConfig `json:"facebook"`
+	Apple                    SocialAuthConfig `json:"apple"`
+	Twitter                  SocialAuthConfig `json:"twitter"`
+	Slack                    SocialAuthConfig `json:"slack"`
+	Discord                  SocialAuthConfig `json:"discord"`
+	SocialAuthRedirectUrl    string           `json:"social_auth_redirect_url"`
+	MaxConnectionPoolSize    int              `json:"max_connection_pool_size"`
+	LockoutPolicy            LockoutPolicy    `json:"lockout_policy"`
+	AdminRoles               []string         `json:"admin_roles"`
+	ReadOnlyRoles            []string         `json:"read_only_roles"`
+}
+
+func DefaultConfig() *Config {
+	return &Config{
+		Host:                  "localhost",
+		Port:                  3001,
+		DatabaseUri:           "",
+		MaxConnectionPoolSize: 10,
+		DisableSignup:         false,
+		DisableEmail:          false,
+		DisablePhone:          true,
+		SessionCookieName:     "genie_session",
+		JWT: JWTConfig{
+			Exp: 2000,
+			Alg: "RS512",
+		},
+		LockoutPolicy: LockoutPolicy{
+			Attempts: 10,
+			For:      60,
+		},
+		Google:                SocialAuthConfig{Enabled: false},
+		Github:                SocialAuthConfig{Enabled: false},
+		Linkedin:              SocialAuthConfig{Enabled: false},
+		Facebook:              SocialAuthConfig{Enabled: false},
+		Apple:                 SocialAuthConfig{Enabled: false},
+		Twitter:               SocialAuthConfig{Enabled: false},
+		Slack:                 SocialAuthConfig{Enabled: false},
+		Discord:               SocialAuthConfig{Enabled: false},
+		SocialAuthRedirectUrl: "http://localhost:5173",
 	}
-	JWT `json:"jwt"`
-	DB  struct {
-		Host     string
-		Port     string
-		User     string
-		Password string
-		Name     string
-		SSLMode  string `default:"disabled"`
-		DSN      string
+}
+
+func New(path string) (*Config, error) {
+	config := DefaultConfig()
+
+	content, err := os.ReadFile(path)
+
+	if err != nil {
+		return nil, err
 	}
-}
 
-var Config AppConfig
-var err error
-
-func init() {
-	// we can use exported func and deal with devMod and pass the value here and do logics
-	if err := godotenv.Load(); err != nil {
-		log.Fatal("error loading .env file")
+	if err = json.Unmarshal(content, &config); err != nil {
+		return nil, err
 	}
-	Config.App.Host = os.Getenv("Host")
-	Config.App.Port = os.Getenv("PORT")
 
-	Config.JWT.SecretKey = os.Getenv("JWT_SECRET")
-	Config.JWT.PrivateKeyPath = os.Getenv("PRIVATE_KEY_PATH")
-	Config.JWT.PublicKeyPath = os.Getenv("PUBLIC_KEY_PATH")
-	Config.JWT.Alg = os.Getenv("JWT_SIGNING_ALGORITHM")
-	Config.JWT.Type = os.Getenv("TYPE")
-
-	Config.DB.Host = os.Getenv("DB_HOST")
-	Config.DB.Port = os.Getenv("DB_PORT")
-	Config.DB.User = os.Getenv("DB_USER")
-	Config.DB.Password = os.Getenv("DB_PASSWORD")
-	Config.DB.Name = os.Getenv("DB_NAME")
-	Config.DB.SSLMode = os.Getenv("DB_SSL_MODE")
-	Config.DB.DSN = os.Getenv("DSN")
-	// Config.DB.DSNUrl = os.Getenv("DSN_URL")
-
-	if strings.ToLower(Config.Type) == enum.Asymmetric {
-		_, ok := os.LookupEnv("PRIVATE_KEY_PATH")
-		_, ok2 := os.LookupEnv("PUBLIC_KEY_PATH")
-		if !ok || !ok2 {
-			log.Fatalln("jwt secret must be provided")
-		}
-
-		Config.PrivateKey, Config.PublicKey, err = crypto.ReadKeys(Config.PrivateKeyPath, Config.PublicKeyPath)
-		if err != nil {
-			log.Fatalln("error ocurred in loading jwt keys")
-		}
-	} else {
-		_, ok := os.LookupEnv("JWT_SECRET")
-		if !ok {
-			log.Fatalln("jwt secret must be provided")
-		}
+	if err = validateSocial(config); err != nil {
+		return nil, err
 	}
+
+	if err = validateCommon(config); err != nil {
+		return nil, err
+	}
+
+	if config.JWT.privateKey, config.JWT.publicKey, err = crypto.ReadKeys(config.JWT.PrivateKeyPath, config.JWT.PublicKeyPath); err != nil {
+		return nil, err
+	}
+
+	return config, nil
 }
 
-func NewDefaultConfig() *AppConfig {
-	return &AppConfig{}
+func validateSocial(config *Config) error {
+
+	if config.Google.Enabled && (config.Google.ClientID == "" || config.Google.SecretID == "") {
+		return ErrGoogleConfig
+	}
+
+	if config.Linkedin.Enabled && (config.Linkedin.ClientID == "" || config.Linkedin.SecretID == "") {
+		return ErrLinkedinConfig
+	}
+
+	if config.Facebook.Enabled && (config.Facebook.ClientID == "" || config.Facebook.SecretID == "") {
+		return ErrFacebookConfig
+	}
+
+	if config.Apple.Enabled && (config.Apple.ClientID == "" || config.Apple.SecretID == "") {
+		return ErrAppleConfig
+	}
+	// ...will continue
+	return nil
 }
 
-func NewConfig() *AppConfig {
-	return &AppConfig{}
-}
+func validateCommon(config *Config) error {
 
-func (c *AppConfig) validateKeys() error {
+	if config.DatabaseUri == "" {
+		return ErrDatabaseURIRequired
+	}
+
+	if config.DisableEmail && config.DisablePhone {
+		return ErrPhoneEmailDisabled
+	}
 
 	return nil
 }
 
-// func (j *JWT) GetSigningKey() interface{} {
-// 	if j.Type == "asymmetric" {
-// 		return j.PrivateKey
-// 	}
-// 	return j.Secret
-// }
+func (j *JWTConfig) GetSignKey() interface{} {
+	return j.privateKey
+}
 
-// func (j *JWT) GetDecodingKey() interface{} {
-// 	if j.Type == "asymmetric" {
-// 		return j.PublicKey
-// 	}
-// 	return j.Secret
-// }
+func (j *JWTConfig) GetDecodeKey() interface{} {
+	return j.publicKey
+}
