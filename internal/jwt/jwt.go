8d0fc17ba1d5bc37630d3be7ce6b2f6b6808768a
Minilik Shumete <minilikshumete@gmail.com>
2025-06-11 23:53:04 +0300

    feat: restructure the folder to make it easy to understand - follow standards

--- internal/jwt/jwt.go
+++ internal/jwt/jwt.go
@@ -5,30 +5,31 @@ import (
 	"time"
 
 	"github.com/golang-jwt/jwt/v4"
-	"github.com/minilikmila/goAuth/config"
-	"github.com/minilikmila/goAuth/model"
-	"github.com/minilikmila/goAuth/utils/crypto"
+	"github.com/minilikmila/standard-auth-go/config"
+	"github.com/minilikmila/standard-auth-go/internal/crypto"
+	"github.com/minilikmila/standard-auth-go/internal/model"
 )
 
 type JWTClaims struct {
 	jwt.RegisteredClaims
-	Name     *string      `json:"name"`
-	Phone    *string      `json:"phone_number,omitempty"`
-	Email    *string      `json:"email"`
-	Metadata *interface{} `json:"metadata,omitempty"`
-	Role     string       `json:"role"`
+	Name     *string     `json:"name"`
+	Phone    *string     `json:"phone_number,omitempty"`
+	Email    *string     `json:"email"`
+	Metadata interface{} `json:"metadata,omitempty"`
+	Role     string      `json:"role"`
+	config   *config.Config
 }
 
-func New(user *model.User, metadata *interface{}) *JWTClaims {
+func New(user *model.User, metadata interface{}, config *config.Config) *JWTClaims {
 	now := time.Now()
 	//  j, _:=time.ParseDuration(config.Config.JWT.Exp)
 	//  ex := time.Now().Add(j).uni
 
 	return &JWTClaims{
 		jwt.RegisteredClaims{
-			Audience:  jwt.ClaimStrings{config.Config.JWT.Audience},
-			Issuer:    config.Config.JWT.Iss,
-			ExpiresAt: jwt.NewNumericDate(now.Add(time.Minute * config.Config.JWT.Exp)),
+			Audience:  jwt.ClaimStrings{config.JWT.Aud},
+			Issuer:    config.JWT.Iss,
+			ExpiresAt: jwt.NewNumericDate(now.Add(time.Minute * config.JWT.Exp)),
 			Subject:   user.ID.String(),
 		},
 		user.Name,
@@ -36,12 +37,13 @@ func New(user *model.User, metadata *interface{}) *JWTClaims {
 		user.Phone,
 		metadata,
 		user.Role,
+		config,
 	}
 }
 
 func (j *JWTClaims) Sign() (token string, err error) {
 
-	jwtAlg := config.Config.JWT.Alg
+	jwtAlg := j.config.JWT.Alg
 
 	signMethod := jwt.GetSigningMethod(jwtAlg)
 	if signMethod == nil {
@@ -55,15 +57,15 @@ func (j *JWTClaims) Sign() (token string, err error) {
 
 	switch signMethod {
 	case jwt.SigningMethodHS256, jwt.SigningMethodHS384, jwt.SigningMethodHS512:
-		return t.SignedString(config.Config.JWT.Secret)
+		return t.SignedString(j.config.JWT.Secret)
 	case jwt.SigningMethodRS256, jwt.SigningMethodRS384, jwt.SigningMethodRS512:
-		privateKey, err := crypto.ParseRSAPrivateKeyFromPemString(config.Config.PrivateKey.([]byte))
+		privateKey, err := crypto.ParseRSAPrivateKeyFromPemString(j.config.JWT.GetSignKey().([]byte))
 		if err != nil {
 			return "", err
 		}
 		return t.SignedString(privateKey)
 	case jwt.SigningMethodES256, jwt.SigningMethodES384, jwt.SigningMethodES512:
-		privateKey, err := crypto.ParseECDSAPrivateKeyFromPemString(config.Config.PrivateKey.([]byte))
+		privateKey, err := crypto.ParseECDSAPrivateKeyFromPemString(j.config.JWT.GetSignKey().([]byte))
 		if err != nil {
 			return "", err
 		}
@@ -74,12 +76,12 @@ func (j *JWTClaims) Sign() (token string, err error) {
 }
 
 // Parse of verify JWT token
-func Decode(token string) (*JWTClaims, error) {
+func Decode(token string, config *config.Config) (*JWTClaims, error) {
 
 	var err error
 	var jwtToken *jwt.Token
 
-	jwtAlg := config.Config.JWT.Alg
+	jwtAlg := config.JWT.Alg
 
 	signingMethod := jwt.GetSigningMethod(jwtAlg)
 	if signingMethod == nil {
@@ -91,11 +93,11 @@ func Decode(token string) (*JWTClaims, error) {
 	switch signingMethod {
 	case jwt.SigningMethodHS256, jwt.SigningMethodHS384, jwt.SigningMethodHS512:
 		jwtToken, err = jwt.ParseWithClaims(token, claims, func(t *jwt.Token) (interface{}, error) {
-			return []byte(config.Config.Secret), nil
+			return []byte(config.JWT.Secret), nil
 		})
 	case jwt.SigningMethodRS256, jwt.SigningMethodRS384, jwt.SigningMethodRS512:
 		jwtToken, err = jwt.ParseWithClaims(token, claims, func(t *jwt.Token) (interface{}, error) {
-			pubKey, err := crypto.ParseRSAPublicKeyFromPemString(config.Config.PublicKey.([]byte))
+			pubKey, err := crypto.ParseRSAPublicKeyFromPemString(config.JWT.GetDecodeKey().([]byte))
 			if err != nil {
 				return nil, err
 			}
@@ -103,7 +105,7 @@ func Decode(token string) (*JWTClaims, error) {
 		})
 	case jwt.SigningMethodES256, jwt.SigningMethodES384, jwt.SigningMethodES512:
 		jwtToken, err = jwt.ParseWithClaims(token, claims, func(t *jwt.Token) (interface{}, error) {
-			pubKey, err := crypto.ParseECDSAPublicKeyFromPemString(config.Config.PublicKey.([]byte))
+			pubKey, err := crypto.ParseECDSAPublicKeyFromPemString(config.JWT.GetDecodeKey().([]byte))
 			if err != nil {
 				return nil, err
 			}
--- model/enum/types.go
+++
@@ -1,8 +0,0 @@
-package enum
-
-const (
-	Asymmetric            string = "asymmetric"
-	Symmetric             string = "symmetric"
-	TokenTypeAccessToken  string = "access_token"
-	TokenTypeRefreshToken string = "refresh_token"
-)
