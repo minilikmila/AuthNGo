fd14fdb289edb9da481ef28d8927b0837c62ed34
Minilik Shumete <minilikshumete@gmail.com>
2025-06-12 15:36:11 +0300

    feat: add middlewares: cors, device-attach log

---
+++ internal/middleware/check-context.go
@@ -0,0 +1,16 @@
+package middleware
+
+import (
+	"errors"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CheckUserContext(ctx *gin.Context, name string) (any, error) {
+	ctx_user, exist := ctx.Get("user_data")
+
+	if !exist {
+		return nil, errors.New("error: context not found")
+	}
+	return ctx_user, nil
+}
