91168a76e90316e094bddb1df3d37cd46ddd60af
Minilik Shumete <minilikshumete@gmail.com>
2025-06-20 23:38:01 +0300

    feat: restructure the design and remove or re-organize previous design

--- internal/migrations/00001_create_users.down.sql
+++
@@ -1,6 +0,0 @@
--- -- This file should undo anything in `up.sql`
--- drop table if exists auth.users;
-
--- drop function if exists auth.set_current_timestamp_updated_at;
-
--- drop schema if exists auth;
--- utils/generate-uuid.go
+++
@@ -1,8 +0,0 @@
-package utils
-
-import uuid "github.com/google/uuid"
-
-func GenerateUUID() *uuid.UUID {
-	uuid, _ := uuid.NewUUID()
-	return &uuid
-}
